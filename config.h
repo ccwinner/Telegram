1a16701cddb36c7516ee6220cb6e96de5df91eff
chenxiao <chenxiao@lianjia.com>
2016-07-30 10:18:43 +0800

    本地配置

---
+++ config.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Telegraph
+//
+//  Created by chenxiao on 16/6/23.
+//
+//
+
+#ifndef Header_h
+#define Header_h
+
+#define SETUP_API_ID(apiId) apiId = 37061;
+#define SETUP_API_HASH(apiHash) apiHash = @"18d9d36fa3d47a46806c649af0cc673d";
+#endif /* Header_h */
